4466b0b52f3a8ed78ff67fb3355aeb1adbdcd3d0
Neupane07 <70212703+Neupane07@users.noreply.github.com>
2021-08-03 12:41:41 +0530

    Update firebase.js
    Removed config code from firebase.js

--- todoWithDb/src/firebase.js
+++ todoWithDb/src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD2HmD6HysJ6G33MjN6Oqgzsq9w_T1wEUs",
-    authDomain: "todolist-69ba4.firebaseapp.com",
-    projectId: "todolist-69ba4",
-    storageBucket: "todolist-69ba4.appspot.com",
-    messagingSenderId: "867259474159",
-    appId: "1:867259474159:web:d88626cc53afb833f87b1a"
+    //config hidden
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
